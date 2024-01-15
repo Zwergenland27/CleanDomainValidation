e5fb009fb5c120164a521db577bcda9487af29e7
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2024-01-15 23:04:18 +0100

    some improvements to nuget package and added comments to validators

--- CleanDomainValidation/Application/QueryValidator.cs
+++ CleanDomainValidation/Application/QueryValidator.cs
@@ -1,8 +1,14 @@
 ﻿namespace CleanDomainValidation.Application;
 
+/// <summary>
+/// Validates <typeparamref name="TUrlParameter"/> to <typeparamref name="TQuery"/>
+/// </summary>
 public abstract class QueryValidator<TUrlParameter, TQuery> : Validator<TQuery>
 	where TUrlParameter : IUrlParameter
 	where TQuery : IQuery
 {
-	public abstract void Configure(TUrlParameter parameter);
+	/// <summary>
+	/// Configures how <paramref name="urlParameter"/> and <paramref name="urlParameter"/> should be validated to create <typeparamref name="TQuery"/> instance
+	/// </summary>
+	public abstract void Configure(TUrlParameter urlParameter);
 }
