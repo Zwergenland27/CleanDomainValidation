5d545a5e8294981720b9710e850b119329349bd7
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2024-01-16 20:31:39 +0100

    Added string conversion support for enums closes #7

---
+++ Playground/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CleanDomainValidation.Application;
+using CleanDomainValidation.Domain;
+
+Params request = new("TIME");
+
+var command = CommandBuilder<Rekord>
+	.AddParameter(request)
+	.ValidateByUsing<Tests>();
+
+if (command.HasFailed)
+{
+	Console.Write(command.Errors[0].Code);
+	return;
+}
+
+Console.WriteLine(command.Value);
+
+internal class Tests : CommandValidator<Params, Rekord>
+{
+	public override void Configure(Params parameter)
+	{
+		Type t = RequiredEnum<Type>(parameter.Type, Error.Validation("1", "2"), Error.Validation("3", "4"));
+
+		CreateInstance(() => new Rekord(t));
+	}
+}
+
+public record Params(string? Type) : IParameter;
+
+public record Rekord(Type t) : ICommand;
+
+
+public enum Type
+{
+	SMS,
+	TIME,
+	Wildschwein
+}
