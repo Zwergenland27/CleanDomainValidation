a4c46df531157fc5a96107f0f65be9ffd8f076fa
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2024-02-05 14:34:29 +0100

    implemented CanFail conversion closes #2

--- CleanDomainValidation/Domain/CanFailOfT.cs
+++ CleanDomainValidation/Domain/CanFailOfT.cs
@@ -1,14 +1,32 @@
 ﻿
 namespace CleanDomainValidation.Domain;
 
+/// <summary>
+/// Can be used instead of returning <typeparamref name="TResult"/> if an error can occur in the method execution
+/// </summary>
 public sealed class CanFail<TResult> : AbstractCanFail, ICanFail<TResult>
 {
 	private TResult _value = default!;
 
 	private bool _valueSet = false;
 
-	public static InvalidOperationException ValueNotSet => new ("The value has not been set yet");
+	/// <summary>
+	/// Error that occurs when the value is accessed but not set yet
+	/// </summary>
+	public static InvalidOperationException ValueNotSet => new ("The value of the result object has not been set yet");
 
+	/// <summary>
+	/// Access the value that should be returned normally
+	/// </summary>
+	/// <remarks>
+	/// This property can only be accessed if the value is set and no errors occured
+	/// </remarks>
+	/// <exception cref="ValueInvalidException">
+	/// The value is set but errors have occured
+	/// </exception>
+	/// <exception cref="ValueNotSetException">
+	/// The value is not set
+	/// </exception>
 	public TResult Value
 	{
 		get
@@ -19,11 +37,32 @@ public sealed class CanFail<TResult> : AbstractCanFail, ICanFail<TResult>
 		}
 	}
 
-
+	/// <summary>
+	/// Assigns <paramref name="value"/> of <typeparamref name="TResult"/> to value of the result
+	/// </summary>
+	/// <remarks>
+	/// Method can be called after a failure has been added to the result but the value cannot be accessed.
+	/// </remarks>
 	public void Succeeded(TResult value)
 	{
 		_value = value;
 		_valueSet = true;
+	}
+
+	/// <summary>
+	/// Converts failure result to <see cref="CanFail{TResult}"/> with <typeparamref name="TOther"/> being the result type
+	/// </summary>
+	public CanFail<TOther> GetFailureAs<TOther>()
+	{
+		return CanFail<TOther>.FromFailure(this);
+	}
+
+	/// <summary>
+	/// Converts failure result to <see cref="CanFail"/>
+	/// </summary>
+	public CanFail GetFailure()
+	{
+		return CanFail.FromFailure(this);
 	}
 
 	/// <summary>
